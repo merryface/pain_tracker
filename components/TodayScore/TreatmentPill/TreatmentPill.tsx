e5d06d3b07b6a6bd8fea6312e2534b2da841570c
Tarik <tarikmerrylees@gmail.com>
2021-07-03 22:45:36 +0100

    treatmentPill: created empty component

---
+++ components/TodayScore/TreatmentPill/TreatmentPill.tsx
@@ -0,0 +1,13 @@
+import styles from './TreatmentPill.module.scss';
+
+interface Props {
+
+};
+
+const TreatmentPill = ({  }: Props): JSX.Element => {
+	return (
+		<section>TreatmentPill</section>
+	);
+};
+
+export default TreatmentPill;
---
+++ components/TodayScore/TreatmentPill/index.tsx
@@ -0,0 +1,13 @@
+import TreatmentPill from './TreatmentPill';
+
+interface Props {
+
+};
+
+const ConnectedTreatmentPill = ({}: Props): JSX.Element => {
+	return (
+		<TreatmentPill />
+	);
+};
+
+export default ConnectedTreatmentPill;
