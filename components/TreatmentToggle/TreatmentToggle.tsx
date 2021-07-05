296f3f72fa27758ec4515d9318b6bcb3e64770ad
Tarik <tarikmerrylees@gmail.com>
2021-07-05 15:36:00 +0100

    TreatmentToggle: added treatment label

--- components/TreatmentToggle/TreatmentToggle.tsx
+++ components/TreatmentToggle/TreatmentToggle.tsx
@@ -6,10 +6,13 @@ interface Props {
 
 const TreatmentToggle = ({  }: Props): JSX.Element => {
 	return (
-		<label className={styles.container}>
-			<input className={   styles.checkbox } type="checkbox" />
-			<span className={ styles.slider }/>
-		</label>
+		<div className={ styles.container }>
+			<p>Treatment 1</p>
+			<label className={styles.pillContainer}>
+				<input className={   styles.checkbox } type="checkbox" />
+				<span className={ styles.slider }/>
+			</label>
+		</div>
 	);
 };
 
