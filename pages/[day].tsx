d0f437b29cb3604d7e05e98d73521217c1d204ab
Jack Standbridge <jack.s.standbridge@gmail.com>
2021-07-23 11:49:08 +0100

    page-routing: remove uneeded code

--- pages/[day].tsx
+++ pages/[day].tsx
@@ -1,12 +1,1 @@
-import { GetServerSideProps } from 'next';
-
-// export const getServerSideProps: GetServerSideProps = async (context) => {
-// 	const day = context.query;
-// 	debugger;
-
-// 	return {
-// 		props: { day }
-// 	};
-// };
-
 export { default } from '../components/Homepage';
